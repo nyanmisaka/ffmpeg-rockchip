2eb6cdc7e5f5b2e507d1a0a6aef4d6570218968a
nyanmisaka <nst799610810@gmail.com>
2023-12-27 22:51:23 +0800

    lavu: add NV15 format support
    Signed-off-by: nyanmisaka <nst799610810@gmail.com>

--- libavutil/pixfmt.h
+++ libavutil/pixfmt.h
@@ -429,6 +429,8 @@ enum AVPixelFormat {
     AV_PIX_FMT_GBRAP14BE,  ///< planar GBR 4:4:4:4 56bpp, big-endian
     AV_PIX_FMT_GBRAP14LE,  ///< planar GBR 4:4:4:4 56bpp, little-endian
 
+    AV_PIX_FMT_NV15,        ///< like P010, but has no zero padding bits, 15bpp, little-endian
+
     AV_PIX_FMT_NB         ///< number of pixel formats, DO NOT USE THIS if you want to link with shared libav* because the number of formats might differ between versions
 };
 
